fee9d2723054a82d04f27c73835f40f4004b013a
MusiCode1 <MusiCode3@gmail.com>
2022-08-08 21:57:19 +0300

    redirect

--- dist/server.js
+++ dist/server.js
@@ -26,7 +26,6 @@ msg_router.post("/send-message", async (req, res, next) => {
     res.end("OK");
 });
 msg_router.use((req, res, next) => {
-    res.location("https://tzoharhalev.org/");
-    res.end();
+    res.redirect("https://tzoharhalev.org/");
 });
 //# sourceMappingURL=server.js.map
--- src/server.ts
+++ src/server.ts
@@ -46,6 +46,5 @@ msg_router.post("/send-message", async (req, res, next) => {
 });
 
 msg_router.use((req, res, next) => {
-    res.location("https://tzoharhalev.org/");
-    res.end();
+    res.redirect("https://tzoharhalev.org/");
 });
