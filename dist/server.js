b93be4ee0300b4f4aefd644d3c64c9c1375e2e7e
MusiCode1 <MusiCode3@gmail.com>
2022-08-08 21:39:35 +0300

    rm dist

--- dist/server.js
+++ dist/server.js
@@ -3,9 +3,16 @@ import { client } from "./whatsapp-api.js";
 export const msg_router = Router();
 msg_router.use(json());
 msg_router.post("/send-message", async (req, res, next) => {
-    if (!req.body.phone || (!req.body.caption && !req.body.file)) {
+    if ((!req.body.phone || !req.body.key) || (!req.body.caption && !req.body.file)) {
         res.status(400);
         res.end();
+        return;
+    }
+    if (req.body.key !== process.env.KEY) {
+        res.status(400);
+        res.json({ massege: "key is not valid" });
+        res.end();
+        return;
     }
     const { caption, file, phone } = req.body;
     try {
@@ -17,5 +24,8 @@ msg_router.post("/send-message", async (req, res, next) => {
         res.end();
     }
     res.end("OK");
+});
+msg_router.use((req, res, next) => {
+    res.location("https://tzoharhalev.org/");
 });
 //# sourceMappingURL=server.js.map
